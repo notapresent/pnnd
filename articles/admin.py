a9581ed4727a8255dd9d076829331f11f87aad92
notapresent <notapresent@gmail.com>
2015-06-06 19:57:02 +0000

    Connect models to admin interface

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Section, Author, Article
+
+
 # Register your models here.
+admin.site.register(Section)
+admin.site.register(Author)
+admin.site.register(Article)
