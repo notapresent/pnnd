2e500fa22f7226ce9511710d8be59a6ad8ce735a
notapresent <notapresent@gmail.com>
2015-06-06 19:34:23 +0000

    Implement NewsItem model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsItem(models.Model):
+    pub_date = models.DateTimeField('date published')
+    is_published = models.BooleanField()
+    is_own = models.BooleanField()
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    keywords = models.CharField(max_length=250)
