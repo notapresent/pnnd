bcf309d5adae7bfc48f8df75cb509a3a50f0ba9f
notapresent <notapresent@gmail.com>
2015-06-06 19:56:37 +0000

    Timezone and i18n settings

--- pnnd/settings.py
+++ pnnd/settings.py
@@ -76,21 +76,16 @@ WSGI_APPLICATION = 'pnnd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
+LANGUAGE_CODE = 'ru-RU'
+TIME_ZONE = 'Europe/Moscow'
+USE_I18N = False
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 
 # Parse database configuration from $DATABASE_URL
