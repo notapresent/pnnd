a1e2bcd23887afe19870c2313d11fc7c8c303c1b
notapresent <notapresent@gmail.com>
2015-06-08 19:36:33 +0000

    Add is_published toadmin list and filters

--- news/admin.py
+++ news/admin.py
@@ -5,8 +5,8 @@ from .models import NewsItem
 
 # Register your models here.
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'pub_date', 'is_own')
-    list_filter = ['pub_date', 'is_own']
+    list_display = ('title', 'pub_date', 'is_own', 'is_published')
+    list_filter = ['pub_date', 'is_own', 'is_published']
     search_fields = ['title', 'body']
 
 admin.site.register(NewsItem, NewsAdmin)
