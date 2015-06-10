14e858c4ed17940ab3dcd643bbe07ab70dc13861
notapresent <notapresent@gmail.com>
2015-06-10 15:40:47 +0000

    Add main page link to header

--- pnnd/urls.py
+++ pnnd/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.main),
+    url(r'^$', views.main, name="home"),
     url(r'^news/', include('news.urls', namespace="news")),
     url(r'^articles/', include('articles.urls', namespace="articles")),
     url(r'^admin/', include(admin.site.urls)),
