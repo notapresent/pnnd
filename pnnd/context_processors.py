62dad074773c2bc42c2daf11ffc26298852ce9db
notapresent <notapresent@gmail.com>
2015-06-10 15:30:18 +0000

    implement right column

--- pnnd/context_processors.py
+++ pnnd/context_processors.py
@@ -7,4 +7,4 @@ def global_vars(request):
     rightcol_sections = ['analytics', 'trend', 'discussion', 'docs',
                          'politfiction']
     rc_articles = {s: Article.last_in_section(s) for s in rightcol_sections}
-    return {'sections': sections, 'right_column_articles': rc_articles}
+    return {'sections': sections, 'right_column_data': rc_articles}
